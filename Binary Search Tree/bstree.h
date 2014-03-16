81c699b77b6e1f72635f7168839c256d832d5698
Syed Shah <emaadshah@gmail.com>
2014-03-15 22:13:17 -0700

    Update bstree.h

--- Binary Search Tree/bstree.h
+++ Binary Search Tree/bstree.h
@@ -1,40 +1,48 @@
 //------------------------------------------------------------
 // Copyright: Syed Shah 2014
-// BSTREE: Insert, Print
+// BSTREE:    Insert, Print, Count Leaves, LCA, Retrieve 
 //------------------------------------------------------------
+
 #ifndef BSTREE_H
 #define BSTREE_H
 #include <iostream>
-#include <fstream>
-// To detect  memory leak
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
 
 using namespace std;
 
 class BSTree
 {
-public:
-	// Constructor
-	BSTree();
-	// Deconstructor
-	~BSTree();
-	// Insert a node 
-	bool insert(int);
-	void print();
-	
-private:
-	struct Node
-	{
-		Node* left;
-		Node* right;
-		int data;
-	};
-	Node *root;
+	public:
+		// Constructor
+		BSTree();
+		// Delete all nodes from the BSTree
+		~BSTree();
+		// Insert a node 
+		bool insert(const int &);
+		void print();
+		// Count number of leaves in the BSTree
+		int countLeaves();
+		// Find the least common ancestor between two nodes
+		void lca(int, int);
 
-	void inOrder(Node*&);
-	void free(Node*&);
+	private:
+		struct Node
+		{
+			Node* left;
+			Node* right;
+			int data;
+		};
+		Node *root;
+
+		// Display the tree in order
+		void inOrder(Node*&);
+		// Count Leaves Helper function
+		int countLeavesHelper(Node*&);
+		// Helper function to find the least common ancestor
+		void lcaHelper(Node*&, const int &, const int &, int&);
+		// Check if data exists in the BSTree
+		bool retrieve(Node*&, const int &);
+		// Helper function for the deconstructor
+		void free(Node*&);
 };
 
 #endif
